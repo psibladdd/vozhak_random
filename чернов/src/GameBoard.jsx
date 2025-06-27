76a8a82aed64f15b236b3a33cca62b874464d68a
Dima <151432491+psibladdd@users.noreply.github.com>
2025-06-27 04:19:46 +0300

    Update GameBoard.jsx

--- чернов/src/GameBoard.jsx
+++ чернов/src/GameBoard.jsx
@@ -14,49 +14,67 @@ const Cube = ({rot, size, color,pos}) => {
         </>
     );
 }
-const Roаd = ({rot,size,pos}) => {
-      const StreetLamp = ({position,isNight}) => {
-return (
-    <group position={position}>
-        <mesh position={[0,0.2,0]}></mesh>
-        <cylindrGeometry args={[0.02,0.02,0.3,16]}/>
-        <mwshBаsicMateriаl color="black"/>
-        <sphereGeometry args= {isNight ? "yellow" : "white"}/>
-         </mesh
->         mesh position= {[
+const Road = ({rot,size,pos}) => {
+    return (
+        <group rotation={rot} position={pos}>
+        <mesh>
+            <boxGeometry args={size}/>
+            <meshBasicMaterial color="#333333"/>
+            <Edges color="black"/>
+        </mesh>
+        <mesh position={[0,0.02,0]}>
+            boxGeometry args={[size[0],size[1]*1.1,size[2]*0.1]}/>
+            <meshBasicMaterial color="yellow"/>
+            </mesh>
+        </group>
+    );
+}
+const StreetLamp = ({position,isNight}) => {
+    return (
+        <group position={position}>
+            <mesh position={[0,0.2,0]}>
+                <cylinderGeometry args={[0.02,0.02,0.2,16]}/>
+                <meshBasicMaterial color="black"/>
+                </mesh>
+
+                <mesh position={[0,0.35,0]}>
+                <sphereGeometry args={[0.05,16,16]}/>
+                <meshBasicMaterial color={isNight ? "yellow" : "white"}/>
+                </mesh>
+        </group>
+    );
+}
 const App = () => {
 
-}
-
-
- const[isNight, setIsNight] = useState(false)
+ const[isNight, setIsNight] = useState(false);
 
     const toggleNight = () => {
     setIsNight(!isNight);
     }
 
     return (
-        <div style ={{ position:'fixed',top:0,left:0,height:'100vh',width:'100vw'}}>
+        <div style={{position: 'fixed', top: 0, left: 0, height: '100vh', width: '100vw'}}>
             <button
-            onClick={toggleNight}>
+                onClick={toggleNight}>
                 Смена
             </button>
-            <Canvas style={{ background: isNight ? "black" : "skyblue",width:"100%",height:"100%"}}>
-                <StreetLamp position={[0,0,1]} isNight={isNight}/>
-                <Cube rot={[0, Math.PI/4, 0]} size={[3, 0.25, 3]} color={"green"} pos={[0,0,0]}/>
+            <Canvas style={{background: isNight ? "black" : "skyblue", width: "100%", height: "100%"}}>
+                <StreetLamp position={[0, 0, 1]} isNight={isNight}/>
+                <Cube rot={[0, Math.PI / 4, 0]} size={[3, 0.25, 3]} color={"green"} pos={[0, 0, 0]}/>
+
+                <Cube rot={[0, Math.PI / 4, 0]} size={[0.8, 1.5, 1]} color={"#3451FF"} pos={[0, 0.7, -1.1]}/>
+                <Road rot={[0,Math.PI/4,0]} pos= {[0,0.1,0]} size={[3,0.1,0.4]} />
+                 <Road rot={[0,-Math.PI/4,0]} pos= {[0,0.1,0]} size={[3,0.1,0.4]} />
                 <OrbitControls
                     minDistance={5}
                     maxDistance={50}
-                    minPolarAngle={Math.PI/4}
-                    maxPolarAngle={Math.PI/4}
-                    />
+                    minPolarAngle={Math.PI/40}
+                    maxPolarAngle={Math.PI/4+Math.PI/8}
+                />
             </Canvas>
         </div>
-        const StreetLamp = ({position})
-        return (
     );
 }
 
-const App = () => {
 export default App;
 
